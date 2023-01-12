921166383728ecec19d178b727ee232b25bf6e95
samiraelshon123 <samiraelshon872@gmail.com>
2023-01-12 12:57:38 +0200

    commit

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,7 +16,7 @@ class RoleSeeder extends Seeder
     public function run()
     {
         //adminstrator
-        $permission1 = [
+        $permission = [
             "Assign_Agent",
             "Add_Ticket",
             "Show_Ticket",
@@ -40,13 +40,36 @@ class RoleSeeder extends Seeder
             "Delete_Ticket_Logs",
 
         ];
-        foreach ($permission1 as $value){
+        foreach ($permission as $value){
             Permission::create([
                 "name"=>$value,
                 "guard_name"=>"web"
             ]);
         }
 
+	 $permission1 = [
+            "Assign_Agent",
+            "Show_Ticket",
+            "Update_Ticket",
+            "Delete_Ticket",
+            "Add_User",
+            "Show_User",
+            "Update_User",
+            "Delete_User",
+            "Add_Category",
+            "Show_Category",
+            "Update_Category",
+            "Delete_Category",
+            "Add_Label",
+            "Show_Label",
+            "Update_Label",
+            "Delete_Label",
+            "Add_Ticket_Logs",
+            "Show_Ticket_Logs",
+            "Update_Ticket_Logs",
+            "Delete_Ticket_Logs",
+
+        ];
         $role1 = [
             "name"=>"admin",
             "guard_name"=>"web"
